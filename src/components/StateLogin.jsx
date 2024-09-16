098df0bd56e2d8973e9e34e559ee65f1ae569232
Ceren <109301665+cerenkuru@users.noreply.github.com>
2024-09-16 15:05:31 +0300

    Add files via upload

---
+++ src/components/StateLogin.jsx
@@ -0,0 +1,60 @@
+import Input from "./Input";
+import { isEmail, isNotEmpty, hasMinLength } from "../util/validation";
+import { useInput } from "../hooks/useInput";
+export default function StateLogin() {
+  const {
+    value: emailValue,
+    handleInputChange: handleEmailChange,
+    handleInputBlur: handleEmailBlur,
+    hasError: emailHasError
+  } = useInput('', (value) => isEmail(value) && isNotEmpty(value));
+
+  const {value: passwordValue, handleInputChange: handlePasswordChange,
+    handleInputBlur: handlePasswordBlur,
+    hasError: passwordHasError
+  } = useInput('',(value) => hasMinLength(value,6))
+
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    if(emailHasError || passwordHasError){
+      return;
+    }
+    console.log(emailValue, passwordValue);
+  }
+
+
+return (
+  <form onSubmit={handleSubmit}>
+    <h2>Login</h2>
+
+    <div className="control-row">
+      <Input
+        label="Email"
+        id="email"
+        type="email"
+        name="email"
+        onBlur={handleEmailBlur}
+        onChange={handleEmailChange}
+        value={emailValue}
+        error={emailHasError && "Please enter a valid email!"}
+      />
+
+      <Input
+        label="Password"
+        type="password"
+        name="password"
+        onChange={handlePasswordChange}
+        onBlur={handlePasswordBlur}
+        value={passwordValue}
+        error={passwordHasError && "Please enter a valid password!"}
+      />
+    </div>
+
+    <p className="form-actions">
+      <button className="button button-flat">Reset</button>
+      <button className="button">Login</button>
+    </p>
+  </form>
+)
+}
